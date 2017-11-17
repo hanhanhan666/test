43b29ac7dbd0b4ab7033d53f4d01148ec9e3316c
hanjiawei <18813073015@163.com>
2017-11-17 16:21:12 +0800

    功能3

--- src/com/koala/controller/UserController.java
+++ src/com/koala/controller/UserController.java
@@ -4,11 +4,8 @@ public class UserController {
 			public static void main(String[] args) {
 				
 				System.out.println("111");
-				System.out.println("222");
 				
-				System.out.println("222");
 				
-				System.out.println("222");
-				System.out.println("222");
+				System.out.println("hanjiawei");
 			}
 }
